a7421215146fbd44124bb332f0fd7f6e74389817
Magnus <magndut@gmail.com>
2024-02-24 16:41:36 +0100

    Fixed width

--- app/modules/Hero.jsx
+++ app/modules/Hero.jsx
@@ -57,7 +57,7 @@ export function Hero() {
 
         <div className="border-b-red border-b-[4px] hero-image opacity-0 translate-y-[100px] text-white flex items-center justify-center lg:col-start-2 lg:row-start-1 mt-10 mb-2 md:mt-[25%] lg:mb-[10%] rounded-md overflow-hidden">
           <Image
-            className="grayscale object-cover aspect-[2/3]"
+            className="grayscale object-cover aspect-[2/3] w-full"
             src="/maria.webp"
             width={521}
             height={782}
@@ -68,7 +68,7 @@ export function Hero() {
         </div>
         <div className="border-b-red border-b-[4px] hero-image opacity-0 translate-y-[-100px] text-white flex items-center justify-center lg:col-start-3 lg:row-start-1 mb-10 mt-2 md:mb-[25%] lg:mt-[10%] rounded-md overflow-hidden">
           <Image
-            className="grayscale object-cover aspect-[2/3]"
+            className="grayscale object-cover aspect-[2/3] w-full"
             src="/arash.webp"
             width={1004}
             height={1508}
